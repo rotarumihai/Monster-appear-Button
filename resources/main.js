370be5a45602e980fc5ba27c37d446177e380349
Mihai <117591224+rotarumihai@users.noreply.github.com>
2023-05-28 21:43:15 +0300

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var audio = document.getElementById("click");
+    audio.play();
+    
+    document.querySelector("body").style.backgroundImage = "url(https://cdn.pixabay.com/photo/2019/11/29/06/32/zombie-4660662_1280.jpg)";
+    document.body.style.backgroundSize = "1300px 1000px";
+    
+ 
+ }
